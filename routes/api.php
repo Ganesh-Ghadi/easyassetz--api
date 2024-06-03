b53db5f2dfb89e7b0253d1f0f1d21dde7306edb9
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2024-06-03 19:11:05 +0530

    first

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+Route::group(['middleware'=>['auth.guest']], function(){
+
+    Route::post('/register', [UserController::class, 'register']);
+    Route::post('/login', [UserController::class, 'login']);
+
+});
